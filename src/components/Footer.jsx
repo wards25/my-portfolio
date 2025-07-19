e4e08ca51a75ac781b41da573e333b99a20cc2e7
wards25 <edwardjohndelbarrio92@gmail.com>
2025-07-19 15:10:07 +0800

    changes in navBar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-center p-4 text-sm">
-      © {new Date().getFullYear()} My Name • Built with React & Tailwind
+      © {new Date().getFullYear()} Edward John Del Barrio
     </footer>
   );
 }
